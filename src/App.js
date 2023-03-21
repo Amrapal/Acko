836f3985f2b8c7233f9330e171181513e9c8e744
Amrapal <dananeamrapal@gmail.com>
2023-03-21 17:57:33 +0530

    edit by amrapal

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './Component/Footer'
 import Products_Health from './Component/Products_Health';
+import NavbarMain from './Component/Navbar';
 
 const App = () => {
   return (
     <div>
+      <NavbarMain/>
       < Products_Health/>
       <Footer/>
     </div>
